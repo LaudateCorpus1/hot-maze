b683f33c40bad581cad8e641dea01c70964601e7
Deleplace <deleplace2015@gmail.com>
2020-09-23 18:49:57 +0200

    Hot Maze B2: Firebase hosting folders, Functions CORS, GCS bucket CORS.

--- B2/backend/cors.go
+++ B2/backend/cors.go
@@ -6,14 +6,15 @@ import (
 	"strings"
 )
 
+// CORS
 func (s Server) accessControlAllowHotMaze(w http.ResponseWriter, r *http.Request) error {
 	origin := r.Header.Get("Origin")
 	origin = strings.TrimSpace(origin)
 
 	// Specific hosts.
 	whiteList := []string{
-		"https://hot-maze.appspot.com",
-		"https://hot-maze.uc.r.appspot.com",
+		"https://hotmaze.io",
+		"https://hot-maze.firebaseapp.com",
 		// For debug.
 		"http://localhost:8080",
 		"http://localhost:8000",
